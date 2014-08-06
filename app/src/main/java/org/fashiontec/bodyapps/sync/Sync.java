f719fad4922a391982ce86a2a905437512d52923
paarandika <paarandika@gmail.com>
2014-08-06 13:05:32 +0530

    Added enum for pic types and formatted code.

--- app/src/main/java/org/fashiontec/bodyapps/sync/Sync.java
+++ app/src/main/java/org/fashiontec/bodyapps/sync/Sync.java
@@ -5,18 +5,7 @@
 
 package org.fashiontec.bodyapps.sync;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.SocketTimeoutException;
-import java.net.URL;
+import android.util.Log;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -29,75 +18,84 @@ import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 
-import android.util.Log;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Class to handle main sync methods
  */
 public class Sync {
 
-    public static String serverID="http://freelayers.org";
+    static final String TAG = Sync.class.getName();
+    public static String baseURL = "http://192.168.1.2:8020";
 
-    public static void setServerID(String serverID) {
-        Sync.serverID = serverID;
+    public static void setBaseURL(String baseURL) {
+        Sync.baseURL = baseURL;
     }
 
     /**
-	 * Method which makes all the POST calls
-	 * 
-	 * @param url
-	 * @param json
-	 * @return
-	 */
-	public HttpResponse POST(String url, String json, int conTimeOut, int socTimeOut) {
-        HttpResponse response=null;
+     * Method which makes all the POST calls
+     *
+     * @param url
+     * @param json
+     * @return
+     */
+    public HttpResponse POST(String url, String json, int conTimeOut, int socTimeOut) {
+        HttpResponse response = null;
         try {
 
-			HttpParams httpParameters = new BasicHttpParams();
-			HttpConnectionParams.setConnectionTimeout(httpParameters,conTimeOut);
-			HttpConnectionParams.setSoTimeout(httpParameters, socTimeOut);
-			HttpClient httpclient = new DefaultHttpClient(httpParameters);
-			HttpPost httpPost = new HttpPost(url);
-			StringEntity se = new StringEntity(json);
-			httpPost.setEntity(se);
-			httpPost.setHeader("Accept", "application/json");
-			httpPost.setHeader("Content-type", "application/json");
-			response= httpclient.execute(httpPost);
-		} catch (Exception e) {
-			Log.d("InputStream", e.getMessage());
-		}
+            HttpParams httpParameters = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(httpParameters, conTimeOut);
+            HttpConnectionParams.setSoTimeout(httpParameters, socTimeOut);
+            HttpClient httpclient = new DefaultHttpClient(httpParameters);
+            HttpPost httpPost = new HttpPost(url);
+            StringEntity se = new StringEntity(json);
+            httpPost.setEntity(se);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            response = httpclient.execute(httpPost);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
 
-		return response;
-	}
+        return response;
+    }
 
-	/**
-	 * Converts the given input stream to a string.
-	 * @param inputStream
-	 * @return
-	 * @throws IOException
-	 */
-	public String convertInputStreamToString(InputStream inputStream)
-			throws IOException {
-		BufferedReader bufferedReader = new BufferedReader(
-				new InputStreamReader(inputStream));
-		String line = "";
-		String result = "";
-		while ((line = bufferedReader.readLine()) != null)
-			result += line;
+    /**
+     * Converts the given input stream to a string.
+     *
+     * @param inputStream
+     * @return
+     * @throws IOException
+     */
+    public String convertInputStreamToString(InputStream inputStream)
+            throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null)
+            result += line;
 
-		inputStream.close();
-		result = result.replaceAll("\"", "");
-		return result;
+        inputStream.close();
+        result = result.replaceAll("\"", "");
+        return result;
 
-	}
+    }
 
-    public int download(String url, String file){
+    public int download(String url, String file) {
 
         try {
             URL u = new URL(url);
             HttpURLConnection c = (HttpURLConnection) u.openConnection();
             c.setConnectTimeout(2000);
-            c.setRequestProperty ("Accept", "application/vnd.valentina.hdf");
+            c.setRequestProperty("Accept", "application/vnd.valentina.hdf");
             c.connect();
 
             FileOutputStream f = new FileOutputStream(new File(file));
@@ -111,30 +109,25 @@ public class Sync {
                 f.write(buffer, 0, len1);
             }
             f.close();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }catch (Exception e){
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
             return -1;
         }
         return 1;
     }
 
-    public HttpResponse GET(String url, int conTimeOut, int socTimeOut){
-        HttpResponse response=null;
+    public HttpResponse GET(String url, int conTimeOut, int socTimeOut) {
+        HttpResponse response = null;
         try {
             HttpParams httpParameters = new BasicHttpParams();
-            HttpConnectionParams.setConnectionTimeout(httpParameters,conTimeOut);
+            HttpConnectionParams.setConnectionTimeout(httpParameters, conTimeOut);
             HttpConnectionParams.setSoTimeout(httpParameters, socTimeOut);
             HttpClient client = new DefaultHttpClient(httpParameters);
             HttpGet request = new HttpGet(url);
-            request.setHeader("Accept","application/json");
+            request.setHeader("Accept", "application/json");
             response = client.execute(request);
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.e(TAG, e.getMessage());
         }
         return response;
 
@@ -152,9 +145,9 @@ public class Sync {
             request.setEntity(se);
             request.setHeader("Accept", "application/json");
             request.setHeader("Content-type", "application/json");
-            response=client.execute(request);
+            response = client.execute(request);
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.e(TAG, e.getMessage());
         }
         return response;
     }
--- app/src/main/java/org/fashiontec/bodyapps/sync/SyncAdapter.java
+++ app/src/main/java/org/fashiontec/bodyapps/sync/SyncAdapter.java
@@ -20,11 +20,7 @@ import org.fashiontec.bodyapps.managers.UserManager;
 import org.fashiontec.bodyapps.models.Measurement;
 import org.fashiontec.bodyapps.models.Person;
 
-import java.security.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 
 /**
  * Manages the automatic sync of measurements with web API
@@ -32,6 +28,7 @@ import java.util.TimeZone;
 public class SyncAdapter extends AbstractThreadedSyncAdapter {
 
     ContentResolver mContentResolver;
+    static final String TAG = SyncAdapter.class.getName();
 
 
     public SyncAdapter(Context context, boolean autoInitialize) {
@@ -49,7 +46,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     public void onPerformSync(Account account, Bundle bundle, String s,
                               ContentProviderClient contentProviderClient,
                               SyncResult syncResult) {
-        Log.d("SyncAdapter", "sync happened");
+        Log.d(TAG, "sync happened");
         Measurement measurement;
 
         String userID = UserManager.getInstance(getContext().getApplicationContext()).getCurrent();
@@ -57,17 +54,12 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
                 getContext().getApplicationContext()).getLastSync(), userID);
         boolean syncOK = true;
 
-        if (list!=null) {
+        if (list != null) {
 
             for (int i = 0; i < list.length; i++) {
                 String out = SyncMeasurement.getMeasurement(list[i],
                         getContext().getApplicationContext(), userID);
-                if (out.equals(list[i])) {
-                    Log.d("SyncAdapter", "get happened");
-//                    SyncPic.getPic(list[i], getContext().getApplicationContext(), 1);
-//                    SyncPic.getPic(list[i], getContext().getApplicationContext(), 2);
-//                    SyncPic.getPic(list[i], getContext().getApplicationContext(), 3);
-                } else {
+                if (!out.equals(list[i])) {
                     syncOK = false;
                     break;
                 }
@@ -79,7 +71,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 
             Person person = PersonManager.getInstance(getContext().getApplicationContext())
                     .getPersonbyID(measurement.getPersonID());
-            boolean syncedOnce= MeasurementManager.getInstance(getContext().getApplicationContext())
+            boolean syncedOnce = MeasurementManager.getInstance(getContext().getApplicationContext())
                     .isSyncedOnce(measurement.getID());
 
             String out = SyncMeasurement.sendMeasurement(measurement, person, syncedOnce, getContext().getApplicationContext());
@@ -90,18 +82,14 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
                         .addMeasurement(measurement);
                 MeasurementManager.getInstance(getContext().getApplicationContext())
                         .setSyncedOnce(measurement.getID());
-            }else {
+            } else {
                 break;
             }
-            Log.d("SyncAdapter", out +"note");
+            Log.d(TAG, out);
         }
 
         if (syncOK) {
-            DateFormat df = DateFormat.getTimeInstance();
-            df.setTimeZone(TimeZone.getTimeZone("gmt"));
-            String gmtTime = df.format(new Date());
-            UserManager.getInstance(getContext().getApplicationContext()).setLastSync(new Date().getTime()+120000);
-            Log.d("SyncAdapter", gmtTime);
+            UserManager.getInstance(getContext().getApplicationContext()).setLastSync(new Date().getTime() + 120000);
         }
 
     }
