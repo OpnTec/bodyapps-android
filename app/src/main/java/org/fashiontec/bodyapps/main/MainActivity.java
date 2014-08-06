67241c0af006794c9dc3f626d95b904e07f7f7f1
paarandika <paarandika@gmail.com>
2014-08-07 00:12:36 +0530

    Formatted the code.

--- app/src/main/java/org/fashiontec/bodyapps/main/MainActivity.java
+++ app/src/main/java/org/fashiontec/bodyapps/main/MainActivity.java
@@ -8,14 +8,11 @@ package org.fashiontec.bodyapps.main;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
-
 import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -23,89 +20,88 @@ import android.widget.Button;
 import org.fashiontec.bodyapps.managers.UserManager;
 
 /**
- * 
  * Landing Screen of the app. All the functions will start from here.
- *
  */
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener {
 
-	private Button create;
-	private Button saved;
-	private Button settings;
-	private Button exit;
+    private Button create;
+    private Button saved;
+    private Button settings;
+    private Button exit;
     private static AlertDialog alertDialog;
 
     public static final String AUTHORITY = "org.fashiontec.bodyapps.sync.provider";
     public static final String ACCOUNT_TYPE = "fashiontec.org";
     public static final String ACCOUNT = "dummyaccount";
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		exit=(Button)findViewById(R.id.main_btn_exit);
-		exit.setOnClickListener(this);
-		saved=(Button)findViewById(R.id.main_btn_saved);
-		saved.setOnClickListener(this);
-		settings=(Button)findViewById(R.id.main_btn_settings);
-		settings.setOnClickListener(this);
-		create=(Button)findViewById(R.id.main_btn_create);
-		create.setOnClickListener(this);
-        if(UserManager.getInstance(getBaseContext().getApplicationContext()).getCurrent()==null){
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        exit = (Button) findViewById(R.id.main_btn_exit);
+        exit.setOnClickListener(this);
+        saved = (Button) findViewById(R.id.main_btn_saved);
+        saved.setOnClickListener(this);
+        settings = (Button) findViewById(R.id.main_btn_settings);
+        settings.setOnClickListener(this);
+        create = (Button) findViewById(R.id.main_btn_create);
+        create.setOnClickListener(this);
+        if (UserManager.getInstance(getBaseContext().getApplicationContext()).getCurrent() == null) {
             dialog("Login", "Go to \"Settings\" and sign in to continue.");
         }
-        if(UserManager.getInstance(getBaseContext().getApplicationContext()).getAutoSync()) {
+        if (UserManager.getInstance(getBaseContext().getApplicationContext()).getAutoSync()) {
             Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
             AccountManager accountManager = (AccountManager) this.getSystemService(ACCOUNT_SERVICE);
             accountManager.addAccountExplicitly(newAccount, null, null);
             ContentResolver.setSyncAutomatically(newAccount, AUTHORITY, true);
             ContentResolver.requestSync(newAccount, "org.fashiontec.bodyapps.sync.provider", Bundle.EMPTY);
         }
-	}
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(UserManager.getInstance(getBaseContext().getApplicationContext()).getCurrent()==null){
+        if (UserManager.getInstance(getBaseContext().getApplicationContext()).getCurrent() == null) {
             saved.setEnabled(false);
             create.setEnabled(false);
-        }else{
+        } else {
             saved.setEnabled(true);
             create.setEnabled(true);
         }
     }
 
     @Override
-	public void onClick(View v) {
-		//Handles the clicks of every Button
-		Intent intent;
-		switch (v.getId()) {
-		case R.id.main_btn_create:
-			intent=new Intent(MainActivity.this, CreateActivity.class);
-			startActivity(intent);
-			break;
-		case R.id.main_btn_saved:
-			intent=new Intent(MainActivity.this, SavedActivity.class);
-			startActivity(intent);
-			break;
-		case R.id.main_btn_settings:
-			intent=new Intent(MainActivity.this, SettingsActivity.class);
-			startActivity(intent);
-			break;
-		case R.id.main_btn_exit:
-			finish();
-			break;
-		}
-		
-	}
-	@Override
-	public void onBackPressed() {
-		finish();
-	}
+    public void onClick(View v) {
+        //Handles the clicks of every Button
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.main_btn_create:
+                intent = new Intent(MainActivity.this, CreateActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.main_btn_saved:
+                intent = new Intent(MainActivity.this, SavedActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.main_btn_settings:
+                intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.main_btn_exit:
+                finish();
+                break;
+        }
 
-    public void dialog(String title, String message){
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    public void dialog(String title, String message) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(title)
@@ -118,7 +114,8 @@ public class MainActivity extends Activity implements OnClickListener{
                                                 int id) {
                                 dialog.cancel();
                             }
-                        });
+                        }
+                );
 
         alertDialog = builder.create();
         alertDialog.show();
