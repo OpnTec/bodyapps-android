b97c313f79472c4996874e2c48d292a363305e4c
paarandika <paarandika@gmail.com>
2014-08-07 00:11:18 +0530

    Disabled "saved" and "create" if user is not logged in.

--- app/src/main/java/org/fashiontec/bodyapps/main/MainActivity.java
+++ app/src/main/java/org/fashiontec/bodyapps/main/MainActivity.java
@@ -9,7 +9,9 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
 
+import android.app.AlertDialog;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -28,14 +30,23 @@ import org.fashiontec.bodyapps.managers.UserManager;
 
 public class MainActivity extends Activity implements OnClickListener{
 
+<<<<<<< HEAD
     private Button create;
     private Button saved;
     private Button settings;
     private Button exit;
+=======
+	private Button create;
+	private Button saved;
+	private Button settings;
+	private Button exit;
+    private static AlertDialog alertDialog;
+>>>>>>> 113b835... Disabled "saved" and "create" if user is not logged in.
 
     public static final String AUTHORITY = "org.fashiontec.bodyapps.sync.provider";
     public static final String ACCOUNT_TYPE = "fashiontec.org";
     public static final String ACCOUNT = "dummyaccount";
+<<<<<<< HEAD
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +61,25 @@ public class MainActivity extends Activity implements OnClickListener{
         settings.setOnClickListener(this);
         create=(Button)findViewById(R.id.main_btn_create);
         create.setOnClickListener(this);
+=======
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		exit=(Button)findViewById(R.id.main_btn_exit);
+		exit.setOnClickListener(this);
+		saved=(Button)findViewById(R.id.main_btn_saved);
+		saved.setOnClickListener(this);
+		settings=(Button)findViewById(R.id.main_btn_settings);
+		settings.setOnClickListener(this);
+		create=(Button)findViewById(R.id.main_btn_create);
+		create.setOnClickListener(this);
+        if(UserManager.getInstance(getBaseContext().getApplicationContext()).getCurrent()==null){
+            dialog("Login", "Go to \"Settings\" and sign in to continue.");
+        }
+>>>>>>> 113b835... Disabled "saved" and "create" if user is not logged in.
         if(UserManager.getInstance(getBaseContext().getApplicationContext()).getAutoSync()) {
             Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
             AccountManager accountManager = (AccountManager) this.getSystemService(ACCOUNT_SERVICE);
@@ -81,11 +111,70 @@ public class MainActivity extends Activity implements OnClickListener{
             break;
         }
 
+<<<<<<< HEAD
     }
     @Override
     public void onBackPressed() {
         finish();
     }
+=======
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(UserManager.getInstance(getBaseContext().getApplicationContext()).getCurrent()==null){
+            saved.setEnabled(false);
+            create.setEnabled(false);
+        }else{
+            saved.setEnabled(true);
+            create.setEnabled(true);
+        }
+    }
 
+    @Override
+	public void onClick(View v) {
+		//Handles the clicks of every Button
+		Intent intent;
+		switch (v.getId()) {
+		case R.id.main_btn_create:
+			intent=new Intent(MainActivity.this, CreateActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.main_btn_saved:
+			intent=new Intent(MainActivity.this, SavedActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.main_btn_settings:
+			intent=new Intent(MainActivity.this, SettingsActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.main_btn_exit:
+			finish();
+			break;
+		}
+		
+	}
+	@Override
+	public void onBackPressed() {
+		finish();
+	}
+>>>>>>> 113b835... Disabled "saved" and "create" if user is not logged in.
 
+    public void dialog(String title, String message){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setIcon(R.drawable.warning)
+                .setCancelable(false)
+                .setNegativeButton("Close",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        alertDialog = builder.create();
+        alertDialog.show();
+    }
 }
